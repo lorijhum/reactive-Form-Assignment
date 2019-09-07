9031d7ab7ab4ab53858757d07f771bf1163a847b
Lori Humphries <lhemailme@aim.com>
2019-09-07 11:29:29 -0400

    adding fields to form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'reactive-Form-Assignment';
+  signupForm: FormGroup;
+  statusArry = ['Stable', 'Critical', 'Finished'];
+  selectedStatus = '';
+
+  ngOnInit(){
+   this.signupForm = new FormGroup({
+     'projectName': new FormControl(null),
+     'email': new FormControl(null),
+     'status': new FormControl(null) 
+   });
+  }
+  onSubmit() {
+    console.log("form submitted");
+    console.log(this.signupForm);
+  }
+ 
 }
