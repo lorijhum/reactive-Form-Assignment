5b350a00c3efad42a0cc1aa828aba9235f7549e9
Lori Humphries <lhemailme@aim.com>
2019-09-08 09:03:24 -0400

    adding validators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormArray } from '@angular/forms';
+import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -11,17 +11,28 @@ export class AppComponent implements OnInit{
   signupForm: FormGroup;
   statusArry = ['Stable', 'Critical', 'Finished'];
   selectedStatus = '';
-
+  
   ngOnInit(){
    this.signupForm = new FormGroup({
-     'projectName': new FormControl(null),
-     'email': new FormControl(null),
+     'projectName': new FormControl(null, [Validators.required, this.forbiddenProjectName]),
+     'email': new FormControl(null, [Validators.required, Validators.email]),
      'status': new FormControl(null) 
    });
+  
   }
   onSubmit() {
     console.log("form submitted");
     console.log(this.signupForm);
   }
+
+  forbiddenProjectName(control: FormControl): {[s: string]: boolean} {
+    if(control.value === 'Test' ) {
+      return {'nameIsForbidden': true};
+    }
+    //note you do not return false, you return null or nothing for false
+    return null;
+  }
+
+  
  
 }
