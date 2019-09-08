ccf5932e5abbb7add6caa5d9c8ac9cfceeae5a70
Lori Humphries <lhemailme@aim.com>
2019-09-08 09:08:10 -0400

    change to async validator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -14,7 +15,8 @@ export class AppComponent implements OnInit{
   
   ngOnInit(){
    this.signupForm = new FormGroup({
-     'projectName': new FormControl(null, [Validators.required, this.forbiddenProjectName]),
+    //  'projectName': new FormControl(null, [Validators.required, this.forbiddenProjectName]),
+     'projectName': new FormControl(null, Validators.required, this.forbiddenProjectName),
      'email': new FormControl(null, [Validators.required, Validators.email]),
      'status': new FormControl(null) 
    });
@@ -25,12 +27,26 @@ export class AppComponent implements OnInit{
     console.log(this.signupForm);
   }
 
-  forbiddenProjectName(control: FormControl): {[s: string]: boolean} {
+  /* forbiddenProjectName(control: FormControl): {[s: string]: boolean} {
     if(control.value === 'Test' ) {
       return {'nameIsForbidden': true};
     }
     //note you do not return false, you return null or nothing for false
     return null;
+  } */
+
+   //creating an async custom validator to simulate reaching out to server for response
+   forbiddenProjectName(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if(control.value === 'Test') {
+          resolve({'nameIsForbidden': true});
+        }else {
+          resolve(null);
+        }
+      }, 1500);
+    });
+    return promise;
   }
 
   
